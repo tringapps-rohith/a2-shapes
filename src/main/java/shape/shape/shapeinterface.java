e60c3cc6701ccc51554a6639debf5b1c010b0d14
tringapps-rohith <123439228+tringapps-rohith@users.noreply.github.com>
2023-02-28 13:42:54 +0530

    Create shapeinterface.java

---
+++ src/main/java/shape/shape/shapeinterface.java
@@ -0,0 +1,13 @@
+package shape.shape;
+
+/**
+ * Hello world!
+ *
+ */
+import java.util.*;
+import java.util.logging.Logger;
+interface Shape
+{
+        double perimeter();
+        double area();
+}
